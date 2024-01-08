d4ed1bc657f5a3de2f4b6df706de41fbc909d270
Brian Stephens <155034293+githubwithbrian@users.noreply.github.com>
2024-01-08 17:37:09 -0500

    This is where I left off during exercise

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,7 +1,11 @@
 // Declare and assign the variables below
-
+let shuttleName = 'Determination';
+let shuttleSpeedMph = 17500;
+let distanceToMarsKm = 225000000;
+let distanceToMoonKm = 38400;
+const milesPerKm = 0.621;
 // Use console.log to print the 'typeof' each variable. Print one item per line.
-
+console.log(typeof 'shuttlename');
 // Calculate a space mission below
 
 // Print the results of the space mission calculations below
