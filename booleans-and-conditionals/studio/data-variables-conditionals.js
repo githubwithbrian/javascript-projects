695a55ab6b32fea21c5d6e4f8f5a85d2d7740bf9
Brian Stephens <155034293+githubwithbrian@users.noreply.github.com>
2024-01-12 00:59:57 -0500

    Brian's booleans and conditionals studio

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -1,6 +1,38 @@
-// Initialize Variables below
-
-// add logic below to verify total number of astronauts for shuttle launch does not exceed 7
+let date = "Monday 2019-03-18";
+let time = "10:05:34 AM";
+let astronautCount = 7;
+if (astronautCount) {
+    console.log("is no greater than 7");
+}
+let astronautStatus = "ready";
+if (astronautStatus){
+    console.log("is ready");
+}
+let averageAstronautMassKg = 80.7;
+let crewMassKG = astronautCount * averageAstronautMassKg;
+let fuelMassKg = 780000;
+let shuttleMassKg = 74842.31;
+let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg;
+if (totalMassKg){
+    console.log("is less than the maximumMassLimit");
+}
+let maximumMassLimit = 850000;
+let fuelTempCelsius = -225;
+if (fuelTempCelsius){
+    console.log("s no less than -300 OR no greater than -150");
+}
+let minimumFuelTemp = -300;
+let maximumFuelTemp = -150;
+let fuelLevel = 100;
+if (fuelLevel){
+    console.log("is sat 100%");
+}
+let weatherStatus = "clear";
+if (weatherStatus){
+    console.log("is clear");
+}
+let preparedForLiftOff = "true";
+// add logic below to verify total number of astronauts for shuttle launch does not exceed 7)
 
 // add logic below to verify all astronauts are ready
 
