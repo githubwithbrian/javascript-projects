1e57061e55ccdb9fb77bde7711be42f90433aac0
Brian Stephens <155034293+githubwithbrian@users.noreply.github.com>
2024-01-22 18:24:37 -0500

    Brian Loop Exercise

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -3,18 +3,33 @@
     b. Print only the ODD values from 3 - 29, one number per line.
     c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
+for (let i = 0; i <= 20; i++) {
+  console.log(i);
+}
 
+for(let i = 3; i<= 29; i = i+=2) {
+  console.log(i);
+}
 
-
+for (let i = 12; i >= -14; i-=2) {
+  console.log(i);
+}
+for (let i = 50; i >= 20; i-=3) {
+  console.log(i);
+}
 
 /*Exercise #2: 
-Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
-
-
-Construct ``for`` loops to accomplish the following tasks:
+Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].*/
+let pro = "LaunchCode";
+let arr = [1, 5, 'LC101', 'blue', 42];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+/*Construct ``for`` loops to accomplish the following tasks:
   a. Print each element of the array to a new line.
   b. Print each character of the string - in reverse order - to a new line. */ 
-
+  let otherArr = [2, 3, 13, 18, -5, 38, -10, 11, 0, 104];
+  let evens = [], odds = [];
 
 
 
