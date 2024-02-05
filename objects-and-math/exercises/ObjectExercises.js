e2dc6948a91435e8fc5b1771c83e15070ee04735
Brian Stephens <155034293+githubwithbrian@users.noreply.github.com>
2024-02-05 05:08:32 -0500

    objectsExercises

--- objects-and-math/exercises/ObjectExercises.js
+++ objects-and-math/exercises/ObjectExercises.js
@@ -12,8 +12,34 @@ let salamander = {
    age: 5
 };
 
+let superChimpTwo = {
+   name: "Brad",
+   species: "Chimpanzee",
+   mass: 11,
+   age: 6
+};
+
+let bigBeagle = {
+   name: "leroy",
+   species: "Beagle",
+   mass: 14,
+   age: 5
+};
+
+let tinyTardigrade = {
+   name: "Almina",
+   species: "Tardigrade",
+   mass: 0.0000000001,
+   age: 1
+}
 
 // After you have created the other object literals, add the astronautID property to each one.
+
+let astronauts = [superChimpOne, salamander, superChimpTwo, bigBeagle]
+let astronautID = [];
+for (let i = 0; i < astronauts.length; i++){
+   astronauts[i][astronautID] = (Math.random(astronauts) * 10);
+}
 
 // Create an array to hold the animal objects.
 
